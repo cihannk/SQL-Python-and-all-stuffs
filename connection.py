4416a71541d90b39c63de18466a0ef92457a4e32
Cihan Kavuk <44194232+cihannk@users.noreply.github.com>
2020-04-02 17:18:12 +0300

    Update connection.py

--- connection.py
+++ connection.py
@@ -3,6 +3,6 @@ import mysql.connector
 connection = mysql.connector.connect(
         host="localhost",
         user="root",
-        password="1c054fe2f",
+        password="yourpw",
         database="schooldb"
     )
